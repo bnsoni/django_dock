01ab1e6e735f044b8ef2dc2c6ce990062152f3c6
bnsoni <bhargavsoni8@gmail.com>
2021-10-17 01:56:16 +0300

    modified image and django app to allow for CORS headers

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ RUN apt-get update \
     && apt-get -y install libpq-dev gcc \
     && pip install psycopg2 \
     && pip install django-oauth-toolkit \
-    && pip install djangorestframework
+    && pip install djangorestframework \
+    && pip install django-cors-headers
 
 ENV PYTHONUNBUFFERED=1
 WORKDIR /code
--- composeexample/settings.py
+++ composeexample/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'users',
     'oauth2_provider',
     'rest_framework',
+    'corsheaders',
 ]
 
 
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'composeexample.urls'
 
